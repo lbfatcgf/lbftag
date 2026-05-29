9d3cd6bd4dab0c117576fa757112e15f0b9c92c7
lbfatcgf <lbfatcgf@outlook.com>
2026-05-29 15:40:31 +0800

    pg

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import allPlugins from "./plugin/all"
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/lbftag/',
   plugins: allPlugins(),
   // build: {
   //   rolldownOptions:{
