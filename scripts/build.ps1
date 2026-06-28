25db0360c7596448f3552748470314d2f85fc39b
YUGEYUE <37266516+lbfatcgf@users.noreply.github.com>
2026-06-28 16:31:39 +0800

    Update build.ps1

--- scripts/build.ps1
+++ scripts/build.ps1
@@ -8,6 +8,6 @@ Write-Host "编译web完成"
 Set-Location ..
 
 $env:GOEXPERIMENT = "jsonv2"
-go build -ldflags="-s -w" -tags=systray_nogtk -o dist/lbftag cmd/main.go
+go build -ldflags="-s -w" -tags=systray_nogtk -o dist/lbftag.exe cmd/main.go
 
 Write-Host "编译go完成"
